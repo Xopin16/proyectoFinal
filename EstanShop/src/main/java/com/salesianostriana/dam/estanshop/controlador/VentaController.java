1db12ad5b9094c29ea700f8900174874e3cbc16a
Jose Ignacio <rivas.fujos20@triana.salesianos.edu>
2022-05-10 21:49:01 +0200

    Agrego el html del carrito y los fragmentos

--- EstanShop/src/main/java/com/salesianostriana/dam/estanshop/controlador/VentaController.java
+++ EstanShop/src/main/java/com/salesianostriana/dam/estanshop/controlador/VentaController.java
@@ -1,8 +1,14 @@
 package com.salesianostriana.dam.estanshop.controlador;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VentaController {
-
+	
+	@GetMapping ("/cesta")
+	public String controladorCarrito() {
+		return "carrito";
+	}
+	
 }
